63057d98efae851fc861476645b4eff6d77d3ad4
DeepaAnchan <deepa.anchan97@gmail.com>
2019-10-16 17:34:28 +0530

    Add files via upload

---
+++ hibernate_many_to_many/src/test/java/com/mycompany/hbernate_many_to_may/AppTest.java
@@ -0,0 +1,20 @@
+package com.mycompany.hbernate_many_to_may;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
