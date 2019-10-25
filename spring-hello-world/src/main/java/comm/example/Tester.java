58727d07a0b6d44c87e3435716b7df6b590737bd
DeepaAnchan <deepa.anchan97@gmail.com>
2019-10-25 15:54:40 +0530

    Add files via upload

---
+++ spring-hello-world/src/main/java/comm/example/Tester.java
@@ -0,0 +1,30 @@
+package comm.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			
+			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+			/*
+			 * Coach tempCoach=context.getBean("theCoach",BaseballCoach.class);
+			 * System.out.println(tempCoach.getDailyWorkout());
+			 */
+
+			Customer theCustomer=context.getBean("customer",Customer.class);
+			System.out.println(theCustomer);
+			Customer theCustomer1=context.getBean("customer",Customer.class);
+			System.out.println(theCustomer==theCustomer1);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
